a2b5391d2fc2b133867a2afb103ebb95c2a20d97
Zeyu <zeyu123@gmail.com>
2021-05-19 10:42:59 +1000

    first commit

---
+++ Tutorial_1.R
@@ -0,0 +1,33 @@
+# Tutorial 1 RMD -----------------------------------------------------------
+
+# This first tutorial will go through how the elementary basics of getting 
+# started with R with a focus on high dimensional time series modelling
+
+# R Basics
+
+
+
+# FUNCTIONS AND VARIABLES
+
+# Ass
+
+
+# DATA STRUCTURES
+
+
+# DATA STRUCTURES FOR TIME SERIES
+
+
+# CONVERTING BETWEEN DIFFERENT FORMATS
+
+
+# ARIMA MODELLING
+# We end this tutorial by finishing with some basic ARIMA modelling
+# There are multiple different implementations available for fitting ARIMA 
+# models
+# 
+
+
+
+
+
